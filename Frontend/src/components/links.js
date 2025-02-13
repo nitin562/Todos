a4de099b119f943efe20248d91f03ef92996b8f7
Nitin <64725520+nitin562@users.noreply.github.com>
2025-02-13 20:44:44 +0530

    Update links.js

--- Frontend/src/components/links.js
+++ Frontend/src/components/links.js
@@ -1,2 +1,11 @@
 const base="https://todos-iiun.onrender.com"
-export const links={}
+export const links={
+login:`${base}/api/auth/login`,
+register:`${base}/api/auth/createUser`,
+clientAll:`${base}/api/auth/fetchClient`,
+AllNotes:`${base}/api/notes/getNotes`,
+AddNotes:`${base}/api/notes/addnote`,
+updateNote:`${base}/api/notes/updatenote`,
+deleteNote:`${base}/api/notes/deletenote`,
+deleteAll:`${base}/api/notes/deleteAll`,
+}
